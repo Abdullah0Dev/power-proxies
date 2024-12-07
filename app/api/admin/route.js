b0b215ebe41e10048d94f59bfc591f09b06a0839
Abdullah - Web Minds <160670132+Abdullah0Dev@users.noreply.github.com>
2024-12-07 22:29:11 +0200

    Code improvements

--- app/api/admin/route.js
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req, res) {
-  console.log("API route hit");
-  res.status(200).json({ message: "Webhook received!" });
-}
