8f13795ae84c9451c0fb0f47227e89ab8cec6c0f
Abdullah - Web Minds <160670132+Abdullah0Dev@users.noreply.github.com>
2024-12-20 20:01:42 +0200

    code improvements & decrease loading time

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -325,7 +325,8 @@ export default function LandingPage() {
                     {faq.answer}
                   </AccordionContent>
                 </AccordionItem>
-              ))}
+              ))} 
+              
             </Accordion>
           </div>
         </div>
